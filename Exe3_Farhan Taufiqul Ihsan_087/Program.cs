7fd5be88d994ab2cb3245cd32b922392f65d89a6
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-11-23 15:31:05 +0700

    Creating class Node

--- Exe3_Farhan Taufiqul Ihsan_087/Program.cs
+++ Exe3_Farhan Taufiqul Ihsan_087/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Exe3_Farhan_Taufiqul_Ihsan_087
 {
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
     internal class Program
     {
         static void Main(string[] args)
